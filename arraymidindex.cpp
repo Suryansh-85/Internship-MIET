8edad18ff0e504f9d8b997adb442876fb519e0d6
Suryansh-85 <123973748+Suryansh-85@users.noreply.github.com>
2024-07-11 01:33:33 -0700

    Create arraymidindex.cpp

---
+++ arraymidindex.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int array[5]={ 10 ,20 ,30 , 40 , 50};
+
+    int index;
+    index=(0+(size(array))/2);
+    cout<<index;
+    return 0;
+}
